fd50f94c0f375df94e3f0510abc96a18fb686e15
sHass13 <101431839+sHass13@users.noreply.github.com>
2022-04-03 21:56:10 +0200

    thisandthat

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -21,7 +21,7 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# TODO: connect to a local postgresql database
+# TODO: connect to a local postgresql database ✔
 
 #----------------------------------------------------------------------------#
 # Models.
