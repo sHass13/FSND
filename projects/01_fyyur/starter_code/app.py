a8df511fd880aa0cae162ab7eab8a390542abce5
sHass13 <101431839+sHass13@users.noreply.github.com>
2022-04-09 22:04:47 +0200

    bd

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -62,6 +62,7 @@ class Shows(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    venue = db.Column(db.String(), nullable=False)
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 # db.create_all() 
 
