8185e293ebd411691891026dd33b211f4854fd69
sHass13 <101431839+sHass13@users.noreply.github.com>
2022-04-19 22:03:40 +0200

    Update app.py

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -231,7 +231,7 @@ def show_venue(venue_id):
     "upcoming_shows_count": 1,
   }
   data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-  venue2show = Venue.query.filter_by(id='venue_id')
+  venue2show = Venue.query.get(venue_id)
 
   dataset = {
             'id': venue2show.id,
